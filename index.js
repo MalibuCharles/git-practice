11600ba754ad3b4cf4ce80d9fb1d1af3f5aa3415
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-02-02 15:38:14 -0500

    cars

---
+++ index.js
@@ -0,0 +1,28 @@
+const { response } = require("express");
+const express = require("express");
+const { request } = require("http");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log(cars);
+});
+
+const cars = [
+  { id: 1, make: "Tesla", year: 2023, model: "x" },
+  { id: 2, make: "Mercedes", year: 2023, model: "G-wagon" },
+  { id: 3, make: "Jeep", year: 2023, model: "wrangler" },
+];
+
+// GET ALL THE CARS
+app.get("/", (request, response) => {
+  response.send(cars);
+});
+
+// GET MY FUTURE TESLA
+app.get("/car/:carId", (req, res) => {
+  console.log("this is my request.params =====>>>>", req.params);
+  // extracting carId from request params
+  const { carId } = req.params;
+  res.send(cars[carId].make);
+});
